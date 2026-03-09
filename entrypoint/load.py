480f88ff2c880017b6e5b143782937a4c6ac5901
Jesse Hao <jehyu77@gmail.com>
2026-03-09 00:20:53 -0400

    small fix to load.py

--- entrypoint/load.py
+++ entrypoint/load.py
@@ -2,11 +2,11 @@ from datasets import load_dataset, concatenate_datasets
 
 # Load in data and combine "train" and "test" into one big dataset
 dataset = load_dataset("tanganke/stanford_cars")
-# totaL: 16,185 images
+# total: 16,185 images
 full = concatenate_datasets([dataset["train"], dataset["test"]]) 
 
 # Take splits 
-split1 = full.train_test_split(test_size=0.30, seed=42)
+split1 = full.train_test_split(test_size=0.30, seed=85)
 split2 = split1["test"].train_test_split(test_size=0.50, seed=42)
 
 train_ds = split1["train"]  # ~11,330
